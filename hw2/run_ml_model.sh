acd71331cb8d990537e0c7321be11058ac7b4e78
Arpita Saha <saha119@antor.cs.purdue.edu>
2026-03-03 10:37:36 -0500

    run script updated

--- hw2/run_ml_model.sh
+++ hw2/run_ml_model.sh
@@ -3,6 +3,6 @@
 # Run the machine learning model for CS 536 Assignment 2, Part 3.
 # After running the run_experiment.sh script to collect data,
 # this script will process the data and train the ML model.
-python3 ML_model.py --csv ./results/q2_goodput_samples.csv --plots plots/ --models models/
+python3 ML_model.py --csv q2_goodput_samples.csv --plots plots/ --models models/
 
 # the generated plots and models will be saved in the specified directories.
